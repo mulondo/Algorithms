7058f4f76e7e996ef15f8d3ecd5035a20f472489
mulondo <mulondo26@gmail.com>
2021-03-02 23:38:42 +0300

    remove insert function and inoder function out of class

--- search/binary_tree.py
+++ search/binary_tree.py
@@ -4,21 +4,40 @@ class Node:
         self.right = None
         self.value = key
 
-    def insert(self,root, key):
-        if root is None:
-            return Node(key)
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
         else:
-            if root.val == key:
-                return root
-            elif root.val < key:
-                root.right = self.insert(root.right, key)
-            else:
-                root.left = self.insert(root.left, key)
-        return root
+            root.left = insert(root.left, key)
+    return root
 
      # A utility function to do inorder tree traversal
-    def inorder(self,root):
-        if root:
-            self.inorder(root.left)
-            print(root.val)
-            self.inorder(root.right)
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+# Driver program to test the above functions
+# Let us create the following BST
+#    50
+#  /     \
+# 30     70
+#  / \ / \
+# 20 40 60 80
+
+r = Node(50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+# Print inoder traversal of the BST
+inorder(r)
