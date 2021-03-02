9571469cd0771fc26789b3eed8928591d1088837
mulondo <mulondo26@gmail.com>
2021-03-02 23:33:24 +0300

    create insert method

--- search/binary_tree.py
+++ search/binary_tree.py
@@ -3,3 +3,15 @@ class Node:
         self.left = None
         self.right = None
         self.value = key
+
+    def insert(self,root, key):
+        if root is None:
+            return Node(key)
+        else:
+            if root.val == key:
+                return root
+            elif root.val < key:
+                root.right = self.insert(root.right, key)
+            else:
+                root.left = self.insert(root.left, key)
+        return root
