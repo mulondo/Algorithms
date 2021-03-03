b5bfb1ed19010890cf9205832b6234a2081fda81
mulondo <mulondo26@gmail.com>
2021-03-03 23:47:55 +0300

    Traverse through 1 to len(arr)

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,2 +1,9 @@
 def insertionSort(arr):
-    
+
+     # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
