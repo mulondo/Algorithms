d739880a0da3070ecb9de4fb5237d7e56a1e1f2b
mulondo <mulondo26@gmail.com>
2021-03-17 12:26:28 +0300

    implement first hast

---
+++ HashMap/hash_map.py
@@ -0,0 +1,5 @@
+keys = [1,2,3,4]
+value = ['a','b','c','e']
+
+hash = {k:v for k,v in zip (keys,value)}
+print(hash)
--- search/binaryTree.js
+++ search/binaryTree.js
@@ -2,6 +2,6 @@ class Node{
     constructor(key)
 }
 
-class roo {
-    
+class root{
+
 }
