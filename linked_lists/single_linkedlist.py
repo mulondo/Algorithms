baa46fde1640a2d34649b6307a55a852486c346c
mulondo <mulondo26@gmail.com>
2021-03-28 13:15:53 +0300

    create a addElement,length,display methods

--- linked_lists/single_linkedlist.py
+++ linked_lists/single_linkedlist.py
@@ -2,3 +2,32 @@ class Node:
     def __init__(self,data=None):
         self.data = data
         self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = Node()
+
+    def addElement(self,data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next!=None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next!=None:
+            total +=1
+            cur = cur.next
+        return total
+
+    def display(self):
+        elements = []
+        cur = self.head
+
+        while cur.next!=None:
+            cur = cur.next
+            elements.append(cur.data)
+        return elements
+        
