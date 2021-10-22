b07f39b2c5fc475a507c3984bdb52697d14b40a0
mulondo <mulondo26@gmail.com>
2021-10-22 23:38:33 +0300

    add employee and headers

--- HashMap/simpleHash.py
+++ HashMap/simpleHash.py
@@ -1,6 +1,23 @@
 headers_array = ['Id', 'Name']
 data = ['0001']
 
+header_array = ['Book', 'Pen']
+employees = [{'Book': 'picfure', 'Pen': 'bic', 'Pencils': 'zin'}]
+new_header = []
+emps = {}
+
+
+for employee in employees:
+    emps = employee
+
+for header in header_array:
+    # if header == emps.get(header):
+    print(header, employee)
+    if header in emps.keys():
+        new_header.append(header)
+        # print(header, emps.get(header))
+print(new_header)
+
 for x, y in zip(headers_array, data):
     if not y:
         y = ''
